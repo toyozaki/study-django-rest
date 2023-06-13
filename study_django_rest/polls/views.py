e10cde8001dbab1ea52ef822d1e64814be4ffb1f
toyozaki <61676668+toyozaki@users.noreply.github.com>
2023-06-13 20:04:29 +0900

    Impl polls app

--- study_django_rest/polls/views.py
+++ study_django_rest/polls/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    # Todo: Fix race condition
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
