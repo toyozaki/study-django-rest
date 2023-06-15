58dc512832f7d048b6fac6d35f5dd2efb26748e5
toyozaki <61676668+toyozaki@users.noreply.github.com>
2023-06-15 23:38:24 +0900

    Finished part1 tutorial

--- study_django_rest/study_django_rest/settings.py
+++ study_django_rest/study_django_rest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "snippets",
 ]
 
 MIDDLEWARE = [
--- study_django_rest/study_django_rest/urls.py
+++ study_django_rest/study_django_rest/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("", include("api.urls")),
+    path("", include("snippets.urls")),
 ]
